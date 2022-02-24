fb312249fd21eef3067045e9a4bc7bf5c158bc2c
heynarmatha <hey.narmatha@gmail.com>
2022-02-24 19:50:50 +0530

    second commit

--- let-var-const.js
+++ let-var-const.js
@@ -5,12 +5,12 @@
 
 //**********let keyword
 
-// let x = 10;
-// if (x == 10) {
-//     let x = 20;
-//     console.log(x); // 20:  reference x inside the block
-// }
-// console.log(x); // 10: reference at the begining of the script
+let x = 10;
+if (x == 10) {
+    let x = 20;
+    console.log(x); // 20:  reference x inside the block
+}
+console.log(x); // 10: reference at the begining of the script
 
 
 //**********let and var global object
