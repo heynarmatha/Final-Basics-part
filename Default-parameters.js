ca67193c3ad44a65f46aafa8d2ade6de9e6b8e34
heynarmatha <hey.narmatha@gmail.com>
2022-02-25 18:00:54 +0530

    Final commit

---
+++ Default-parameters.js
@@ -0,0 +1,45 @@
+// JavaScript Default Parameters
+
+// assignment operator (=) and the default value after the parameter name to set a default value for that parameter
+
+function message(message='Hi') {
+    console.log(message);
+}
+
+message(); // 'Hi'
+message(undefined);// 'Hi'
+message('Hello') // 'Hello'
+// ***************************************************************
+
+function noMsg(message) {
+    console.log(message);
+}
+
+noMsg(); // undefined
+
+
+// ***************************************************************
+
+function messageTernary(message) {
+    message = typeof message !== 'undefined' ? message : 'Hi';
+    console.log(message);
+}
+messageTernary(); // 'Hi'
+
+
+// ***************************************************************
+
+function add(a, b) {
+    return a + b;
+}
+
+let sum = add;
+
+function average(a, b, sum) {
+    return sum(a, b) / 2;
+}
+
+let result = average(10, 20, sum);
+
+console.log(result); // 15
+
--- let-var-const.js
+++ let-var-const.js
@@ -5,34 +5,34 @@
 
 //**********let keyword
 
-// let x = 10;
-// if (x == 10) {
-//     let x = 20;
-//     console.log(x); // 20:  reference x inside the block
-// }
-// console.log(x); // 10: reference at the begining of the script
+let x = 10;
+if (x == 10) {
+    let x = 20;
+    console.log(x); // 20:  reference x inside the block
+}
+console.log(x); // 10: reference at the begining of the script
 
 
 //**********let and var global object
 
-// var a = 10;
-// console.log(window.a); // 10
+var a = 10;
+console.log(window.a); // 10
 
 
-// let b = 20;
-// console.log(window.b); // undefined
+let b = 20;
+console.log(window.b); // undefined
 
 
 //**********var callback function
 
-// for (var i = 0; i < 5; i++) {
-//     function number(i){
-//         setTimeout(function result() {
-//             console.log(i);
-//         }, i*1000);
-//     }
-//     number(i);
-// }
+for (var i = 0; i < 5; i++) {
+    function number(i){
+        setTimeout(function result() {
+            console.log(i);
+        }, i*1000);
+    }
+    number(i);
+}
 
 //output
 
@@ -44,11 +44,11 @@
 
 // **********let and callback function in a for loop
 
-// for (let i = 0; i < 5; i++) {
-//     setTimeout(function () {
-//         console.log(i);
-//     }, 1000);
-// }
+for (let i = 0; i < 5; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, 1000);
+}
 
 //output
 
@@ -62,15 +62,15 @@
 
 // 1....The var keyword allows you to redeclare a variable without any issue:
 
-// var counter = 0;
-// var counter;
-// console.log(counter); // 0
+var counter = 0;
+var counter;
+console.log(counter); // 0
 
 // 2.... a variable using the let keyword will result in an error:
 
-// let counter = 0;
-// let counter;
-// console.log(counter);
+let counter = 0;
+let counter;
+console.log(counter);
 
 //error
 
@@ -78,29 +78,29 @@
 
 // ****************Temporal death zone (TDZ)******************************
 
-// let log = function () {
-//     console.log(message); // message declared later
-// };
-// let message= 'Hello'; 
-// log(); 
+let log = function () {
+    console.log(message); // message declared later
+};
+let message= 'Hello'; 
+log(); 
 
 
 // TDZ starts
 
-// console.log(typeof myVar); // undefined
-// console.log(typeof message); // ReferenceError
-// let message; // TDZ ends
-// var myVar;
+console.log(typeof myVar); // undefined
+console.log(typeof message); // ReferenceError
+let message; // TDZ ends
+var myVar;
 
 // ****************************Variable scopes
 
 //---------------------------var ----------------------
 
-// for (var i = 0; i < 5; i++) {
-// 	console.log("Inside:", i);
-// }
+for (var i = 0; i < 5; i++) {
+	console.log("Inside:", i);
+}
 
-// console.log("Outside:", i);
+console.log("Outside:", i);
 
 //output
 // Inside: 0 
@@ -112,11 +112,11 @@
 
 //---------------------------let----------------------
 
-// for (let i = 0; i < 5; i++) {
-// 	console.log("Inside:", i);
-// }
+for (let i = 0; i < 5; i++) {
+	console.log("Inside:", i);
+}
 
-// console.log("Outside:", i); //   Uncaught ReferenceError: i is not defined
+console.log("Outside:", i); //   Uncaught ReferenceError: i is not defined
 
 //output
 
