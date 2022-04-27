fc9a372d5bc5145a708182a1a5cb076e7b9fb7fe
heynarmatha <hey.narmatha@gmail.com>
2022-04-27 11:08:54 +0530

    updated

--- Default-parameters.js
+++ Default-parameters.js
@@ -2,44 +2,42 @@
 
 // assignment operator (=) and the default value after the parameter name to set a default value for that parameter
 
-function message(message='Hi') {
-    console.log(message);
+function message(message = "Hi") {
+  console.log(message);
 }
 
 message(); // 'Hi'
-message(undefined);// 'Hi'
-message('Hello') // 'Hello'
+message(undefined); // 'Hi'
+message("Hello"); // 'Hello'
+
 // ***************************************************************
 
 function noMsg(message) {
-    console.log(message);
+  console.log(message);
 }
 
 noMsg(); // undefined
 
-
 // ***************************************************************
 
 function messageTernary(message) {
-    message = typeof message !== 'undefined' ? message : 'Hi';
-    console.log(message);
+  message = typeof message !== "undefined" ? message : "Hi";
+  console.log(message);
 }
 messageTernary(); // 'Hi'
-
 
 // ***************************************************************
 
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 let sum = add;
 
 function average(a, b, sum) {
-    return sum(a, b) / 2;
+  return sum(a, b) / 2;
 }
 
 let result = average(10, 20, sum);
 
 console.log(result); // 15
-
